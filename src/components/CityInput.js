e9edb95a973addc2386aed867f49c9a544973a72
SSAnalyst <ssshinde9403@gmail.com>
2023-09-07 21:01:57 +0530

    firsrt commit

---
+++ src/components/CityInput.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './CityInput.css';
+
+export default class CityInput extends React.Component {
+  render(props) {
+    const onKlickHandler = async e => {
+      e.persist();
+      const eventKey = e.which ? e.which : e.keyCode;
+      const city = e.target.value;
+
+      if (eventKey === 13) {
+        if (/^[a-zA-ZäöüÄÖÜß ]+$/.test(city)) {
+          e.target.classList.add('loading');
+
+          if (await this.props.makeApiCall(city)) e.target.placeholder = 'Enter city name here';
+          else e.target.placeholder = 'City not found, Please try again...';
+        } else e.target.placeholder = 'Invalid input';
+        e.target.classList.remove('loading');
+        e.target.value = '';
+      }
+    };
+
+    const style = {
+      top: this.props.city ? '-380px' : '-20px',
+      width: '600px',
+      display: 'inline-block',
+      padding: '10px 0px 10px 30px',
+      lineHeight: '120%',
+      position: 'relative',
+      borderRadius: '20px',
+      outline: 'none',
+      fontSize: '20px',
+      transition: 'all 0.5s ease-out'
+    };
+
+    return (
+      <input
+        className='city-input'
+        style={style}
+        type='text'
+        placeholder='Enter city name here'
+        onKeyPress={onKlickHandler}
+      />
+    );
+  }
+}
